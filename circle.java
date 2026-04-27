8d8ad7594e1333a38d98d9d9096372df1eec34d5
linoysadot <linisadot@gmail.com>
2026-04-27 12:42:51 +0300

    Update circle.java

--- circle.java
+++ circle.java
@@ -2,11 +2,7 @@ public class circle {
     public double radius;
 
 
-//    public double c(double pie){
-//       return 3.14*(radius*radius);
-//
-//    }
     public void printradius(){
-        System.out.println("the redius is " +3.14*(radius*radius));
+        System.out.println("the redius is " +Math.PI*(radius*radius));
     }
 }
